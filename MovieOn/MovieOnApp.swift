c545bafbe0f127100f7a80952a75a41f8e0d43a0
María Pérez <123765659+mariaperez15@users.noreply.github.com>
2024-12-17 18:15:03 +0100

    Initial Commit

---
+++ MovieOn/MovieOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieOnApp.swift
+//  MovieOn
+//
+//  Created by María Pérez  on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieOnTests/MovieOnTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieOnTests.swift
+//  MovieOnTests
+//
+//  Created by María Pérez  on 17/12/24.
+//
+
+import Testing
+@testable import MovieOn
+
+struct MovieOnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
